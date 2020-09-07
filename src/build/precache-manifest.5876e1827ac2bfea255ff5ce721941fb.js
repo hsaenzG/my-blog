30f92aa8541e5545fc9930b013c69f3ef759a6d2
Hazel Saenz <hazel.saenz@gmail.com>
2020-09-06 20:09:37 -0600

    Curso de React y node.js

---
+++ src/build/precache-manifest.5876e1827ac2bfea255ff5ce721941fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89952c38f7987534f8d5eef5cd731bcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16b46ab8ea30cec00cb",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "47e9c73ae04919e86ca7",
+    "url": "/static/js/2.121faa7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.121faa7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16b46ab8ea30cec00cb",
+    "url": "/static/js/main.1d900ea4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
